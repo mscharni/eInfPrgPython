612910b75e9e76369229d739cd3b7059fad4c0ba
Scharnitzky Miklós <m.scharnitzky@gmail.com>
2022-01-13 17:33:54 +0100

    update

--- 2016_maj_otszaz/otszaz_list.py
+++ 2016_maj_otszaz/otszaz_list.py
@@ -3,13 +3,12 @@
 # 6. feladat: Határozza meg, hogy a bekért darabszámot vásárolva egy termékből mennyi a fizetendő összeg!
 #   A feladat megoldásához készítsen függvényt ertek néven, amely a darabszámhoz a fizetendő összeget rendeli!
 def ertek(db):
-    ossz = 0
-    if db == 1:
-        ossz += 500
+    if db > 2:
+        ossz = 500 + 450 + 400*(db-2)
     elif db == 2:
-        ossz += 500 + 450
+        ossz = 500 + 450
     else:
-        ossz += 500 + 450 + 400*(db-2)
+        ossz = 500
     return ossz
 
 kosarak=[]
@@ -17,45 +16,42 @@ kosarak=[]
 # 1. feladat: Olvassa be és tárolja el a penztar.txt fájl tartalmát!
 print("\n1. feladat")
 sorszam = 0
-kosar = {}  # kezdetben üres a kosár
+kosar = [["F",0]]  # kezdetben üres a kosár, de belerakunk két fiktív terméket (a kosár végére végére), hogy a keresés funkció működjön
+# egy termék ['termék neve', darab] formában kerüla  kosárba
 with open("penztar.txt", "r") as fileBe:
     for sor in fileBe:
         termek = sor.strip()
         if termek == "F":
-            # kosár fizetés
-            sorszam += 1
-            ossz_db = 0
-            ossz_ar = 0
-            # összeszámoljuk a termékek darabszámát és az egyes termékek darabszám után fizetendő összeget
-            for termek in kosar:
-                ossz_db += kosar[termek]
-                ossz_ar += ertek(kosar[termek])
-            # a kiegészítő információk neveit aláhúzásjellel kezdjük, hogy megkülönböztethessük a termékek neveitől
-            kosar["_sorszam"] = sorszam       # letároljuk a kosárba a vásárló sorszámát
-            kosar["_ossz_db"] = ossz_db       # letároljuk a kosárba a összes termék darabszámát
-            kosar["_ossz_ar"] = ossz_ar       # letároljuk a kosárba a teljes fizetendő összeget
+            # letároljuk a kész kosarak között
             kosarak.append(kosar)
             # új kosarat nyitunk (kiürítjuk a péntárosnál lévő kosarat)
-            kosar = {}
+            kosar = [["F",0]]
         else:
-            try:
-                # ha van már a kosárban ilyen termék, akkor növeljük a darabszámát
-                kosar[termek] +=1  
-            except KeyError:
-                # ha nincs, akkor létrehozzuk a kosárban a terméket
-                kosar[termek] = 1
+            # megnézzük, hogy van-e már a kosárban ilyen termék
+            i = 0
+            while kosar[i][0] != termek and kosar[i][0] != "F":
+                i += 1
+            # ha nincs a kosárban ilyen termék, akkor belerakjuk
+            if kosar[i][0] == "F":
+                kosar.insert(0,[termek, 1])
+            else:
+                # van, tehát növeljük a darabszámot
+                kosar[i][1] +=1  
 
 print("Az adatok beolvasva a 'penztar.txt' állományból.")
-
+print(kosarak)
 # 2. feladat: Határozza meg, hogy hányszor fizettek a pénztárnál!
 print("\n2. feladat")
-# a kifizetések száma ugyanayyi, mint a kosár sorszáma 
-print(f"A kifizetések száma: {sorszam}")
+# a kifizetések száma ugyanayyi, mint a kosarak száma 
+print(f"A kifizetések száma: {len(kosarak)}")
 
 # 3. feladat: Írja a képernyőre, hogy az első vásárlónak hány darab árucikk volt a kosarában!
 print("\n3. feladat")
-# az első korás a nulladik
-print(f"Az első vásárló {kosarak[0]['_ossz_db']} darab árucikket vásárolt.")
+# az első korás a nulladik, végigmegyünk az összes trméken és összeadjuk a darabszámokat
+db = 0
+for termek in kosarak[1]:
+    db += termek[1]
+print(f"Az első vásárló {db} darab árucikket vásárolt.")
 
 # 4. feladat: Kérje be a felhasználótól egy vásárlás sorszámát, egy árucikk nevét és egy darabszámot!
 #   A következő három feladat megoldásánál ezeket használja fel!
@@ -70,19 +66,24 @@ db = int(input("Adja meg a vásárolt darabszámot! "))
 print("\n5. feladat")
 elso = -1
 utso = -1
-vasaroltak = 0
+hanyszor = 0
+idx = 0
 for kosar in kosarak:
-    # ha a kosárban van adott termék
-    if termek in kosar.keys():
-        vasaroltak +=1
+    # megkeressük, hogy van-e a kosárban az adott termékből
+    i = 0
+    while kosar[i][0] != termek and kosar[i][0] != "F":
+        i += 1
+    # ha van már a kosárban ilyen termék, akkor növeljük a darabszámát
+    if kosar[i][0] != "F":
+        hanyszor +=1
         # elsőként?
         if elso == -1:
-            elso = kosar["_sorszam"]
-        utso = kosar["_sorszam"]            
-
-print(f"Az első vásárlás sorszáma: {elso}")
-print(f"Az utolsó vásárlás sorszáma: {utso}")
-print(f"{vasaroltak} vásárlás során vettek belőle")
+            elso = idx
+        utso = idx
+    idx += 1
+print(f"Az első vásárlás sorszáma: {elso+1}")
+print(f"Az utolsó vásárlás sorszáma: {utso+1}")
+print(f"{hanyszor} vásárlás során vettek belőle")
 
 # 6. feladat: Határozza meg, hogy a bekért darabszámot vásárolva egy termékből mennyi a fizetendő összeg!
 #   A feladat megoldásához készítsen függvényt ertek néven, amely a darabszámhoz a fizetendő összeget rendeli!
@@ -97,12 +98,18 @@ print("\n7. feladat")
 kosar = kosarak[sorszam-1]
 for termek in kosar:
     # csak a termékeket kell kiírni
-    if termek[0] != "_": 
-        print(f"{termek} {kosar[termek]}")
+    if termek[0] != "F": 
+        print(f"{termek[0]} {termek[1]}")
 
 # 8. feladat: Készítse el az osszeg.txt fájlt, amelybe soronként az egy-egy vásárlás alkalmával fizetendő összeg kerüljön a kimeneti mintának megfelelően!
 print("\n8. feladat")
-with open("osszeg2.txt", "w") as fileKi:
+with open("osszeg3.txt", "w") as fileKi:
+    idx = 1
     for kosar in kosarak:
-        fileKi.write(f"{kosar['_sorszam']}: {kosar['_ossz_ar']}\n")
-print("Adatok kiírva az 'osszeg2.txt' állományba")
+        # kosárbeli termékek összes ára
+        osszar = -500  # a "F" fiktív termékért nem fizetünk
+        for termek in kosar:
+            osszar += ertek(termek[1])
+        fileKi.write(f"{idx}: {osszar}\n")
+        idx +=1
+print("Adatok kiírva az 'osszeg3.txt' állományba")
