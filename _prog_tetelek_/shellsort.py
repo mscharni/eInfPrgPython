4fc667321bd47d8b2fb3fa80b6f8e0d74368515e
Scharnitzky Miklós <m.scharnitzky@gmail.com>
2022-02-08 21:51:23 +0100

    Create shellsort.py

---
+++ _prog_tetelek_/shellsort.py
@@ -0,0 +1,26 @@
+def shell_rendezes(lista):
+    gap = 1
+    n = len(lista)
+    while gap*2 <= n:
+        gap *= 2
+    gap = gap - 1
+    while gap > 0:
+        i = 0
+        while i <= gap and i + gap < n:
+            j = i + gap
+            while j < n:
+                elem = lista[j]
+                index = j - gap
+                while index > -1 and elem < lista[index]:
+                    lista[index + gap] = lista[index]
+                    index = index - gap
+                # print(lista)
+                lista[index + gap] = elem
+                j += gap
+            i += 1
+        gap = gap // 2
+    
+lista=[63,54,33,45,23,99,43,10,35,87]
+print(f"Rendezetlen lista = {lista}")
+shell_rendezes(lista)
+print(f"Rendezett lista   = {lista}")
